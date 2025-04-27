7631877553596bdb48ce4ac296c96c6a08989948
your name1 <you@example.com>
2025-04-28 08:40:53 +0900

    숙제완료

--- js/condition6exe.js
+++ js/condition6exe.js
@@ -5,36 +5,35 @@
 //4.반복문(537페이지 연습.읽어도 보고 연습도 해보고)
 
 // 1.
-// let num1 = prompt("첫번째 수를 입력하세요");
-// let num2 = prompt("두번째 수를 입력하세요");
-// console.log(num1,num2);
-// if(num1 > num2){
-//   console.log("두 수중에서 큰값은" + num1 +"입니다.");
-// }
-// else if(num1 == num2){
-//   console.log("두개의 숫자가 같습니다.");
-// }
-// else{
-//   console.log("두 수중에서 큰 값은" + num2 + "입니다.");
-// }
+let num1 = prompt("첫번째 수를 입력하세요");
+let num2 = prompt("두번째 수를 입력하세요");
+console.log(num1,num2);
+if(num1 > num2){
+  console.log("두 수중에서 큰값은" + num1 +"입니다.");
+}
+else if(num1 == num2){
+  console.log("두개의 숫자가 같습니다.");
+}
+else{
+  console.log("두 수중에서 큰 값은" + num2 + "입니다.");
+}
 //2.
-// let num_1 = 30 + Math.floor(Math.random() * 31);
-// let num_2 = 30 + Math.floor(Math.random() * 31);
-// console.log(num_1,num_2);
-// if(num_1 > num_2)
-// {
-//   console.log("2개의 수중에서 큰값은" + num_1 +",작은값은"+ num_2 + "입니다.");
-// }
-// else if(num_1 == num_2){
-//   console.log("두개의 숫자가 같습니다.");
-// }
-// else{
-//   console.log("2개의 수중에서 큰값은," + num_2 +",작은값은"+num_1 + "입니다.");
-// }
+let num_1 = 40 + Math.floor(Math.random() * 31);
+let num_2 = 40 + Math.floor(Math.random() * 31);
+console.log(num_1,num_2);
+if(num_1 > num_2)
+{
+  console.log("2개의 수중에서 큰값은" + num_1 +",작은값은"+ num_2 + "입니다.");
+}
+else if(num_1 == num_2){
+  console.log("두개의 숫자가 같습니다.");
+}
+else{
+  console.log("2개의 수중에서 큰값은," + num_2 +",작은값은"+num_1 + "입니다.");
+}
 //3
-let result = 30 + Math.floor(Math.random() * 71);
+let result =  Math.floor(Math.random() * 71);
 console.log(result);
-// result = Math.floor(result/10);
 
 if(result>= 95) { 
   console.log("A+");
